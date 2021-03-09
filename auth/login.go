c768237f9178464c162483e7ef62b27770cb07db
Anastasia Morozova <morozovaanastasiaa2001@gmail.com>
2021-03-09 21:08:58 +0300

    fix cookie

--- auth/login.go
+++ auth/login.go
@@ -65,6 +65,7 @@ func (h *LoginHandler) Login(c echo.Context) *echo.HTTPError {
 		Name:    "SID",
 		Value:   key,
 		Expires: time.Now().Add(10 * time.Hour),
+		SameSite: http.SameSiteNoneMode,
 	}
 
 	Store[key] = uid
