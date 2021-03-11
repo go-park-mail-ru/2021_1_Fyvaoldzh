baa4610d9c67467dece4ee256d9f32ae84bce223
MeHighLove <mikhailov_danya@mail.ru>
2021-03-11 16:17:38 +0300

    test 50%

--- events/events_test.go
+++ events/events_test.go
@@ -1,10 +1,17 @@
 package events
 
 import (
+	"bytes"
 	"kudago/models"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"reflect"
 	"sync"
 	"testing"
+
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/require"
 )
 
 var ExpectedEvents = models.Events{
@@ -22,6 +29,12 @@ var ExpectedEventType = models.Events{
 }
 
 var ExpectedNewEvent = models.Event{1, "Пример", "Место", "Пример без картинки", "12:00", "Примерное метро", "Примерная улица", "Музей", ""}
+
+var EventWithNoImage = models.Events{{1, "Пример", "Место", "Пример без картинки", "12:00", "Примерное метро", "Примерная улица", "Музей", ""}}
+
+var ExpectedEventImage = models.Event{1, "Пример", "Место", "Пример без картинки", "12:00", "Примерное метро", "Примерная улица", "Музей", "1.jpg"}
+
+var EventCreate = models.Event{1, "Пример", "Место", "Пример без картинки", "12:00", "Примерное метро", "Примерная улица", "Музей", "1.jpg"}
 
 func TestDeleteByIDOK(t *testing.T) {
 	t.Parallel()
@@ -56,8 +69,10 @@ func TestGetOneEventByIDOK(t *testing.T) {
 		Mu:     &sync.Mutex{},
 	}
 
-	if !reflect.DeepEqual(handler.GetOneEventByID(128), ExpectedEvent) {
-		t.Errorf("expected: [%v], got: [%v]", ExpectedEvent, handler.GetOneEventByID(128))
+	event, err := handler.GetOneEventByID(128)
+
+	if !reflect.DeepEqual(event, ExpectedEvent) || err != nil {
+		t.Errorf("expected: [%v], got: [%v]", ExpectedEvent, event)
 	}
 }
 
@@ -68,8 +83,10 @@ func TestGetOneEventByIDERROR(t *testing.T) {
 		Mu:     &sync.Mutex{},
 	}
 
-	if !reflect.DeepEqual(handler.GetOneEventByID(5), models.Event{}) {
-		t.Errorf("expected: [%v], got: [%v]", models.Event{}, handler.GetOneEventByID(5))
+	event, err := handler.GetOneEventByID(5)
+
+	if !reflect.DeepEqual(event, models.Event{}) && err == nil {
+		t.Errorf("expected: [%v], got: [%v]", models.Event{}, event)
 	}
 }
 
@@ -129,4 +146,99 @@ func TestCreateNewEventERROR(t *testing.T) {
 		t.Error("Your id didn't generate correct!")
 	}
 
+}
+
+func TestSaveImage(t *testing.T) {
+	t.Parallel()
+	handler := Handlers{
+		Events: EventWithNoImage,
+		Mu:     &sync.Mutex{},
+	}
+	img, err := os.Open("2913aa38efbe34ebdb5a1d642dfa29d8.jpg")
+	if err != nil {
+		t.Error("No such file!")
+	}
+	handler.SaveImage(img, 1)
+	if !reflect.DeepEqual(handler.Events[0], ExpectedEventImage) {
+		t.Errorf("expected: [%v], got: [%v]", ExpectedEventImage, handler.Events[0])
+	}
+}
+
+func setupEcho(t *testing.T, url, method string) (echo.Context,
+	Handlers) {
+	e := echo.New()
+	var req *http.Request
+	switch method {
+	case http.MethodPost:
+		f, _ := EventCreate.MarshalJSON()
+		req = httptest.NewRequest(http.MethodGet, url, bytes.NewBuffer(f))
+	case http.MethodGet:
+		req = httptest.NewRequest(http.MethodGet, url, nil)
+	}
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath(url)
+
+	uh := Handlers{
+		Events: models.BaseEvents,
+		Mu:     &sync.Mutex{},
+	}
+	return c, uh
+}
+
+func TestHandler_Main(t *testing.T) {
+	c, uh := setupEcho(t, "/api/v1/", http.MethodGet)
+
+	err := uh.GetAllEvents(c)
+	require.Equal(t, nil, err)
+}
+
+func TestHandler_OneEventOK(t *testing.T) {
+	c, uh := setupEcho(t, "/api/v1/event/:id", http.MethodGet)
+	c.SetParamNames("id")
+	c.SetParamValues("127")
+
+	err := uh.GetOneEvent(c)
+	require.Equal(t, nil, err)
+}
+
+func TestHandler_OneEventERROR(t *testing.T) {
+	c, uh := setupEcho(t, "/api/v1/event/:id", http.MethodGet)
+	c.SetParamNames("id")
+	c.SetParamValues("aaa")
+
+	err := uh.GetOneEvent(c)
+	require.NotEqual(t, nil, err)
+}
+
+func TestHandler_OneEvent1ERROR(t *testing.T) {
+	c, uh := setupEcho(t, "/api/v1/event/:id", http.MethodGet)
+	c.SetParamNames("id")
+	c.SetParamValues("545654")
+
+	err := uh.GetOneEvent(c)
+	require.NotEqual(t, nil, err)
+}
+
+/*func TestHandler_GetImageOK(t *testing.T) {
+	c, uh := setupEcho(t, "api/v1/event/:id/image", http.MethodGet)
+	c.SetParamNames("id")
+	c.SetParamValues("127")
+
+	err := uh.GetImage(c)
+	require.NotEqual(t, nil, err)
+}*/
+
+func TestHandler_Events(t *testing.T) {
+	c, uh := setupEcho(t, "/api/v1/event", http.MethodGet)
+	c.QueryParams().Add("typeEvent", "Музей")
+	err := uh.GetEvents(c)
+	require.Equal(t, err, nil)
+}
+
+func TestHandler_Create(t *testing.T) {
+	c, uh := setupEcho(t, "/api/v1/create", http.MethodPost)
+
+	err := uh.Create(c)
+	require.Equal(t, c.JSON(http.StatusOK, EventCreate), err)
 }
