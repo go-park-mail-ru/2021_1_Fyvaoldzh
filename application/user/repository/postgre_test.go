63c39286fed80a88b232ab2c1b6d4bdf487a5a1e
Anastasia Morozova <morozovaanastasiaa2001@gmail.com>
2021-04-15 11:29:00 +0300

    user repo tests

--- application/user/repository/postgre_test.go
+++ application/user/repository/postgre_test.go
@@ -1,5 +1,25 @@
 package repository
 
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"kudago/application/models"
+	"kudago/pkg/logger"
+	"log"
+	"net"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/jackc/pgmock"
+	"github.com/jackc/pgproto3/v2"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 /*
 import (
@@ -149,6 +169,274 @@ func TestUserDatabase_IsExisting(t *testing.T) {
 }
 
 
- */
+*/
 
+var (
+	userId   uint64 = 1
+	str             = "str"
+	login           = "login"
+	password        = "123456"
+)
 
+var testRegData = &models.RegData{
+	Login:    login,
+	Password: password,
+}
+
+var testUserBack = &models.User{
+	Id:       userId,
+	Login:    login,
+	Password: password,
+}
+
+var testUserData = &models.UserData{
+	Id:    userId,
+	Login: login,
+}
+
+func setUp(t *testing.T) *pgxpool.Pool {
+	script := &pgmock.Script{
+		Steps: pgmock.AcceptUnauthenticatedConnRequestSteps(),
+	}
+	script.Steps = append(script.Steps, pgmock.ExpectMessage(&pgproto3.Query{String: "SELECT id FROM users WHERE login = $1"}))
+	script.Steps = append(script.Steps, pgmock.SendMessage(&pgproto3.RowDescription{
+		Fields: []pgproto3.FieldDescription{
+			pgproto3.FieldDescription{
+				Name:                 []byte("id"),
+				TableOID:             0,
+				TableAttributeNumber: 0,
+				DataTypeOID:          23,
+				DataTypeSize:         8,
+				TypeModifier:         -1,
+				Format:               0,
+			},
+			pgproto3.FieldDescription{
+				Name:                 []byte("name"),
+				TableOID:             0,
+				TableAttributeNumber: 0,
+				DataTypeOID:          23,
+				DataTypeSize:         60,
+				TypeModifier:         -1,
+				Format:               0,
+			},
+			pgproto3.FieldDescription{
+				Name:                 []byte("login"),
+				TableOID:             0,
+				TableAttributeNumber: 0,
+				DataTypeOID:          23,
+				DataTypeSize:         60,
+				TypeModifier:         -1,
+				Format:               0,
+			},
+			pgproto3.FieldDescription{
+				Name:                 []byte("password"),
+				TableOID:             0,
+				TableAttributeNumber: 0,
+				DataTypeOID:          23,
+				DataTypeSize:         60,
+				TypeModifier:         -1,
+				Format:               0,
+			},
+		},
+	}))
+	script.Steps = append(script.Steps, pgmock.SendMessage(&pgproto3.DataRow{
+		Values: [][]byte{[]byte("1")},
+	}))
+	script.Steps = append(script.Steps, pgmock.SendMessage(&pgproto3.CommandComplete{CommandTag: []byte("SELECT 1")}))
+	script.Steps = append(script.Steps, pgmock.SendMessage(&pgproto3.ReadyForQuery{TxStatus: 'I'}))
+	script.Steps = append(script.Steps, pgmock.ExpectMessage(&pgproto3.Terminate{}))
+
+	ln, err := net.Listen("tcp", "127.0.0.1:")
+	require.NoError(t, err)
+	defer ln.Close()
+
+	serverErrChan := make(chan error, 1)
+	go func() {
+		defer close(serverErrChan)
+
+		conn, err := ln.Accept()
+		if err != nil {
+			serverErrChan <- err
+			return
+		}
+		defer conn.Close()
+
+		err = conn.SetDeadline(time.Now().Add(time.Second))
+		if err != nil {
+			serverErrChan <- err
+			return
+		}
+
+		err = script.Run(pgproto3.NewBackend(pgproto3.NewChunkReader(conn), conn))
+		if err != nil {
+			serverErrChan <- err
+			return
+		}
+	}()
+
+	parts := strings.Split(ln.Addr().String(), ":")
+	host := parts[0]
+	port := parts[1]
+	connStr := fmt.Sprintf("sslmode=disable host=%s port=%s", host, port)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	pool, err := pgxpool.Connect(ctx, connStr)
+	require.NoError(t, err)
+	return pool
+}
+
+func TestUserDatabase_AddError(t *testing.T) {
+	pool := setUp(t)
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sugar := l.Sugar()
+	zap.NewAtomicLevelAt(zapcore.DebugLevel)
+
+	h := NewUserDatabase(pool, logger.NewLogger(sugar))
+	_, err = h.Add(testRegData)
+
+	assert.Error(t, err)
+}
+
+func TestUserDatabase_ChangeAvatarError(t *testing.T) {
+	pool := setUp(t)
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sugar := l.Sugar()
+	zap.NewAtomicLevelAt(zapcore.DebugLevel)
+
+	h := NewUserDatabase(pool, logger.NewLogger(sugar))
+	err = h.ChangeAvatar(userId, str)
+
+	assert.Error(t, err)
+}
+
+func TestUserDatabase_AddToPreferencesError(t *testing.T) {
+	pool := setUp(t)
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sugar := l.Sugar()
+	zap.NewAtomicLevelAt(zapcore.DebugLevel)
+
+	h := NewUserDatabase(pool, logger.NewLogger(sugar))
+	err = h.AddToPreferences(userId)
+
+	assert.Error(t, err)
+}
+
+func TestUserDatabase_IsExistingNoRows(t *testing.T) {
+	pool := setUp(t)
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sugar := l.Sugar()
+	zap.NewAtomicLevelAt(zapcore.DebugLevel)
+
+	h := NewUserDatabase(pool, logger.NewLogger(sugar))
+	_, err = h.IsExisting(str)
+	// returns err.NoRows
+	// wtf?!
+
+	assert.Nil(t, err)
+}
+
+func TestUserDatabase_IsCorrectError(t *testing.T) {
+	pool := setUp(t)
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sugar := l.Sugar()
+	zap.NewAtomicLevelAt(zapcore.DebugLevel)
+
+	h := NewUserDatabase(pool, logger.NewLogger(sugar))
+	_, err = h.IsCorrect(testUserBack)
+
+	assert.Error(t, err)
+}
+
+func TestUserDatabase_UpdateError(t *testing.T) {
+	pool := setUp(t)
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sugar := l.Sugar()
+	zap.NewAtomicLevelAt(zapcore.DebugLevel)
+
+	h := NewUserDatabase(pool, logger.NewLogger(sugar))
+	err = h.Update(userId, testUserData)
+
+	assert.Error(t, err)
+}
+
+func TestUserDatabase_GetByIdOwnError(t *testing.T) {
+	pool := setUp(t)
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sugar := l.Sugar()
+	zap.NewAtomicLevelAt(zapcore.DebugLevel)
+
+	h := NewUserDatabase(pool, logger.NewLogger(sugar))
+	_, err = h.GetByIdOwn(userId)
+
+	assert.Error(t, err)
+}
+
+func TestUserDatabase_IsExistingEmail(t *testing.T) {
+	pool := setUp(t)
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sugar := l.Sugar()
+	zap.NewAtomicLevelAt(zapcore.DebugLevel)
+
+	h := NewUserDatabase(pool, logger.NewLogger(sugar))
+	_, err = h.IsExistingEmail(str)
+	// тож false с nil
+
+	assert.Nil(t, err)
+}
+
+func TestUserDatabase_IsExistingUserIdError(t *testing.T) {
+	pool := setUp(t)
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sugar := l.Sugar()
+	zap.NewAtomicLevelAt(zapcore.DebugLevel)
+
+	h := NewUserDatabase(pool, logger.NewLogger(sugar))
+	err = h.IsExistingUserId(userId)
+	// тож false с nil
+
+	assert.Error(t, err)
+}
+
+func TestUserDatabase_GetUsersError(t *testing.T) {
+	pool := setUp(t)
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sugar := l.Sugar()
+	zap.NewAtomicLevelAt(zapcore.DebugLevel)
+
+	h := NewUserDatabase(pool, logger.NewLogger(sugar))
+	_, err = h.GetUsers(1)
+	// ErrNoRows ???
+
+	assert.Nil(t, err)
+}
